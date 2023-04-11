b9fe97327b22a0c08191089c53de4f5e078015be
ajh123 <ajh123@users.noreply.github.com>
2023-04-11 14:18:32 +0100

    Actually implement the road builder.

--- common/src/main/java/tk/minersonline/mineways/item/RoadBuilderItem.java
+++ common/src/main/java/tk/minersonline/mineways/item/RoadBuilderItem.java
@@ -1,17 +1,22 @@
 package tk.minersonline.mineways.item;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import tk.minersonline.mineways.MineWaysMod;
 
 public class RoadBuilderItem extends Item {
 
-	public RoadBuilderItem(Settings properties) {
-		super(properties);
+	public RoadBuilderItem() {
+		super(new Item.Settings().group(MineWaysMod.MAIN_TAB).maxDamage(256));
 	}
 	
 
@@ -19,9 +24,42 @@ public class RoadBuilderItem extends Item {
 	public ActionResult useOnBlock(ItemUsageContext context) {
         World world = context.getWorld();
         BlockPos pos = context.getBlockPos();
-        BlockState state = world.getBlockState(pos);
-        
-		world.setBlockState(pos, Blocks.DIRT_PATH.getDefaultState());
-		return ActionResult.SUCCESS;
+		if (world.isClient()) {
+			return ActionResult.SUCCESS;
+		}
+		ServerPlayerEntity player = (ServerPlayerEntity) context.getPlayer();
+		
+		if (player != null && player.canModifyAt(world, pos)) {
+			ItemStack mainStack = player.getStackInHand(Hand.MAIN_HAND);
+			ItemStack stack = player.getStackInHand(Hand.OFF_HAND);
+			if (!(stack.getItem() instanceof BlockItem)) {
+				player.sendMessageToClient(
+					Text.translatable("mineways.gui.text.road_builder.blockIncompatible", Text.translatable(stack.getTranslationKey())), 
+					false
+				);
+				return ActionResult.FAIL;
+			}
+			Block block = Block.getBlockFromItem(stack.getItem());
+
+			int radius = 1; // Change this to adjust the radius
+			for (BlockPos target : BlockPos.iterateOutwards(pos, radius, 0, radius)) {
+				if (world.getBlockState(target).isOf(block)) {
+					continue;
+				}
+				if (!stack.isEmpty()) {
+					if (!player.isCreative()) {
+						stack.decrement(1);
+						mainStack.damage(1, player, (p) -> {
+							p.sendToolBreakStatus(Hand.MAIN_HAND);
+						});
+						world.breakBlock(target, true, player);
+					}
+					world.setBlockState(target, block.getDefaultState());
+				}
+			}
+
+			return ActionResult.SUCCESS;
+		}
+		return ActionResult.FAIL;
 	}
 }
--- common/src/main/java/tk/minersonline/mineways/setup/item/ItemRegistry.java
+++ common/src/main/java/tk/minersonline/mineways/setup/item/ItemRegistry.java
@@ -9,8 +9,7 @@ import tk.minersonline.mineways.item.RoadBuilderItem;
 
 public class ItemRegistry {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MineWaysMod.MOD_ID, Registry.ITEM_KEY);
-    public static final RegistrySupplier<Item> ROAD_BUILDER = ITEMS.register("road_builder", () ->
-            new RoadBuilderItem(new Item.Settings().group(MineWaysMod.MAIN_TAB)));
+    public static final RegistrySupplier<Item> ROAD_BUILDER = ITEMS.register("road_builder", () -> new RoadBuilderItem());
 	
 	public static void init() {
 		// Register our items, and initalise the static class variables.
