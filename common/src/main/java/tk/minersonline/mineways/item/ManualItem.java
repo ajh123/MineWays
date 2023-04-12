033e9513962d50ea56ba5d8e7ddd240296e4098c
ajh123 <ajh123@users.noreply.github.com>
2023-04-12 11:54:58 +0100

    Try to add the Markdown Manual!

---
+++ common/src/main/java/tk/minersonline/mineways/item/ManualItem.java
@@ -0,0 +1,18 @@
+ package tk.minersonline.mineways.item;
+
+ import li.cil.manual.api.ManualModel;
+ import li.cil.manual.api.prefab.item.AbstractManualItem;
+ import net.minecraft.item.Item;
+ import tk.minersonline.mineways.MineWaysMod;
+
+ public class ManualItem { //extends AbstractManualItem {
+
+ 	public ManualItem() {
+ 		//super(new Item.Settings().group(MineWaysMod.MAIN_TAB));
+ 	}
+
+//	 @Override
+//	 protected ManualModel getManualModel() {
+//		 return null;
+//	 }
+ }
--- common/src/main/java/tk/minersonline/mineways/setup/item/ItemRegistry.java
+++ common/src/main/java/tk/minersonline/mineways/setup/item/ItemRegistry.java
@@ -5,11 +5,13 @@ import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.item.Item;
 import net.minecraft.util.registry.Registry;
 import tk.minersonline.mineways.MineWaysMod;
+import tk.minersonline.mineways.item.ManualItem;
 import tk.minersonline.mineways.item.RoadBuilderItem;
 
 public class ItemRegistry {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MineWaysMod.MOD_ID, Registry.ITEM_KEY);
-    public static final RegistrySupplier<Item> ROAD_BUILDER = ITEMS.register("road_builder", () -> new RoadBuilderItem());
+    public static final RegistrySupplier<Item> ROAD_BUILDER = ITEMS.register("road_builder", RoadBuilderItem::new);
+//	 public static final RegistrySupplier<Item> MANUAL_MAIN = ITEMS.register("manual_main", ManualItem::new);
 	
 	public static void init() {
 		// Register our items, and initalise the static class variables.
